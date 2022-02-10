cdbb4512bf6f99057ec9cfda9d0b9d44b0c18b28
Deividwb <bernardinodeivid@gmail.com>
2022-02-09 23:42:33 -0300

    Topicos básicos Parte 2

---
+++ teste/script.js
@@ -0,0 +1,38 @@
+const obj = {
+    id: 53,
+    date: "2021-10-20",
+    items: [
+        {
+            description: "Celular",
+            price: 1499.99,
+            quantity: 1
+        },
+        {
+            description: "Mouse",
+            price: 100.0,
+            quantity: 2
+        }
+    ],
+    client: {
+        name: "Maria Red",
+        email: "maria@gmail.com",
+        active: true
+    }
+};
+
+function subTotal({price, quantity}){
+    return price*quantity
+}
+
+console.log(subTotal(obj.items[0]))
+
+function total({items}){
+     let soma=0
+     for(let i=0;i<items.length;i++){
+         soma= soma+ subTotal(items[i])
+     }
+     return soma
+}
+
+
+console.log(total(obj))
